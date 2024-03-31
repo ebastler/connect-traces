d08668a047eadc5f214bb321b46b8700d7c8f379
elpekenin <elpekenin@elpekenin.dev>
2024-04-01 00:23:30 +0200

    Check for same layer, hopefully handle delta_x=0 now

--- plugins/connect.py
+++ plugins/connect.py
@@ -1,16 +1,29 @@
 # This file is published under MIT License
 # Copyright (c) 2024 Pablo Martínez (elpekenin)
 
-import os
-from typing import Tuple
+from pathlib import Path
+from typing import Optional, Tuple
 
 import pcbnew as P
+import wx
 
+INF = float("inf")  # slope of a vertical line
 Point = Tuple[int, int]
+
+THIS = Path(__file__)
+ICON = THIS.parent.parent.resolve() / "connect.png"
+
+
+def error(msg: str):
+    """Show popup with error message and quit with error code."""
+    dlg = wx.MessageDialog(None, msg, "Error", wx.OK | wx.ICON_ERROR)
+    dlg.ShowModal()
+    dlg.Destroy()
+    return 1
 
 
 class TrackProxy:
-    """Proxy a track, for easier coord manipulation."""
+    """Proxy a track, for pythonic names and little convenience."""
 
     def __init__(self, track: P.PCB_TRACK):
         self._track = track
@@ -40,31 +53,96 @@ class TrackProxy:
 
         delta_x, delta_y = end_x - start_x, end_y - start_y
 
+        if delta_x == 0:
+            return INF
+
         return delta_y / delta_x
 
-
-def line_props(track: TrackProxy):
-    """Get the `y = mx+n` representation of a track."""
-
-    x, y = track.start
-
-    m = track.slope
-    n = y - track.slope * x
-
-    return m, n
+    @property
+    def layer(self):
+        return self._track.GetLayer()
 
 
-def find_intersection(a: TrackProxy, b: TrackProxy) -> Point:
+class Horizontal:
+    """Represent a line in 2D space."""
+
+    def __init__(self, track: TrackProxy):
+        _, self.y = track.start
+
+
+class Vertical:
+    """Represent a line in 2D space."""
+
+    def __init__(self, track: TrackProxy):
+        self.x, _ = track.start
+
+
+class Slanted:
+    """Represent a line in 2D space."""
+
+    def __init__(self, track: TrackProxy):
+        x, y = track.start
+        self.m = track.slope
+        self.n = y - self.m * x
+
+
+class LineFactory:
+    """Represent a line in 2D space."""
+
+    def __init__(self, track: TrackProxy):
+        slope = track.slope
+
+        if slope == 0:
+            return self.Horizontal(track)
+
+        if slope == INF:
+            return self.Vertical(track)
+
+        return self.Slanted(track)
+
+
+def find_intersection(track_a: TrackProxy, track_b: TrackProxy) -> Optional[Point]:
     """Find the intersection of two tracks."""
 
-    m1, n1 = line_props(a)
-    m2, n2 = line_props(b)
+    line_a = LineFactory(track_a)
+    line_b = LineFactory(track_a)
 
-    if m1 == m2:
-        raise ValueError("Lines are parallel and wont connect")
+    if isinstance(line_a, Horizontal) and isinstance(line_b, Horizontal):
+        return None
 
-    x = (n2 - n1) / (m1 - m2)
-    y = m1 * x + n1
+    elif isinstance(line_a, Horizontal) and isinstance(line_b, Vertical):
+        x = line_b.x
+        y = line_a.y
+
+    elif isinstance(line_a, Horizontal) and isinstance(line_b, Slanted):
+        y = line_a.y
+        x = (y - line_b.n) / line_b.m
+
+    elif isinstance(line_a, Vertical) and isinstance(line_b, Horizontal):
+        x = line_a.x
+        y = line_b.y
+
+    elif isinstance(line_a, Vertical) and isinstance(line_b, Vertical):
+        return None
+
+    elif isinstance(line_a, Vertical) and isinstance(line_b, Slanted):
+        x = line_a.x
+        y = line_b.m * x + line_b.n
+
+    elif isinstance(line_a, Slanted) and isinstance(line_b, Horizontal):
+        y = line_b.y
+        x = (y - line_a.n) / line_a.m
+
+    elif isinstance(line_a, Slanted) and isinstance(line_b, Vertical):
+        x = line_b.x
+        y = line_a.m * x + line_a.n
+
+    elif isinstance(line_a, Slanted) and isinstance(line_b, Slanted):
+        m1, n1 = line_a.m, line_a.n
+        m2, n2 = line_b.m, line_b.n
+
+        x = (n2 - n1) / (m1 - m2)
+        y = m1 * x + n1
 
     # points in KiCAD are ints, cast them
     # done here, at the very end, to avoid carrying rounding errors
@@ -95,17 +173,22 @@ class ConnectTraces(P.ActionPlugin):
         self.category = "Modify PCB"
         self.description = self.__class__.__doc__
         self.show_toolbar_button = True
-        self.icon_file_name = os.path.join(os.path.dirname(__file__), "./connect.png")
+        self.icon_file_name = ICON
 
     def Run(self):
         pcb = P.GetBoard()
 
         selected_tracks = [TrackProxy(track) for track in pcb.GetTracks() if track.IsSelected()]
         if len(selected_tracks) != 2:
-            raise ValueError("Wrong amount of tracks, must select 2.")
+            return error("Wrong amount of tracks, must select 2.")
 
-        # actual logic, extend them to their intersection point
         a, b = selected_tracks
+        if a.layer != b.layer:
+            return error("Tracks are on different layers.")
+
         intersection = find_intersection(a, b)
+        if intersection is None:
+            return error("Lines are parallel and wont connect.")
+
         extend_to(a, intersection)
         extend_to(b, intersection)
